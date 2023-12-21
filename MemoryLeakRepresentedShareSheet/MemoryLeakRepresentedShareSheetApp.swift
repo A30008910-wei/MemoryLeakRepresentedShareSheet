ad8b612ec827e7ce063b1f11f3dff1ffa54d0dd9
Wei Sun <wsun@agl.com.au>
2023-12-21 15:05:20 +1100

    Initial Commit

---
+++ MemoryLeakRepresentedShareSheet/MemoryLeakRepresentedShareSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLeakRepresentedShareSheetApp.swift
+//  MemoryLeakRepresentedShareSheet
+//
+//  Created by Wei Sun on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLeakRepresentedShareSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
